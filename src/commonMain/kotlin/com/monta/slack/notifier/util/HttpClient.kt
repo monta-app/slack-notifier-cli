b94af6259cd23676042fd47e37495be1cef014f4
Jesper Terkelsen <jete@monta.com>
2026-02-18 19:01:25 +0100

    fix: add ExperimentalNativeApi opt-in for Platform.osFamily

--- src/commonMain/kotlin/com/monta/slack/notifier/util/HttpClient.kt
+++ src/commonMain/kotlin/com/monta/slack/notifier/util/HttpClient.kt
@@ -4,12 +4,13 @@ import io.ktor.client.*
 import io.ktor.client.engine.curl.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlin.experimental.ExperimentalNativeApi
 import kotlin.native.OsFamily
 import kotlin.native.Platform
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
-@OptIn(ExperimentalSerializationApi::class)
+@OptIn(ExperimentalSerializationApi::class, ExperimentalNativeApi::class)
 val client by lazy {
     HttpClient(Curl) {
         engine {
